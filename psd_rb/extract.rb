344c97c9747ba396b48ead7eab1b0fbb463945ea
Mickael <mickael@beingenious.com>
2014-05-13 17:27:58 +0200

    added pp and benchmarking
    Signed-off-by: Mickael <mickael@beingenious.com>

--- psd_rb/extract.rb
+++ psd_rb/extract.rb
@@ -1,5 +1,15 @@
 require 'psd'
 require 'benchmark'
+require 'pp'
 
-psd = PSD.new('./psds/rmn_v3.psd')
-psd.parse!
+def parse file
+  psd = PSD.new(file)
+  psd.parse!
+
+  pp psd.tree.to_hash
+end
+
+
+Benchmark.bm(7) do |x|
+  x.report("parse:") {parse './psds/rmn_v3.psd'}
+end
